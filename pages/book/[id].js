66f2c52be599bc50178e7e464ccd6c4a910c8d97
Mauricio Perez <delineamientocam@gmail.com>
2023-12-09 11:06:26 -0500

    'initial'

--- pages/book/[id].js
+++ pages/book/[id].js
@@ -1,7 +1,22 @@
 import { useRouter } from "next/router";
-export default function()
+/*
+export async function generateStaticParams() 
 {
     const url = useRouter()
     const itemId = url.query.id
+    console.log(itemId)
+    const posts = await fetch(`https://fakestoreapi.com/products/${itemId}`)
+    .then((res) => res.json())
+   
+    return posts.map((post) => ({
+      props:{slug: post.slug},
+    }))
+  }
+  */
+export default function()
+{
+
+   const url = useRouter()
+   const itemId = url.query.id
     return(<h2>This is the id: {itemId}</h2>)
 }
